b6fbf0f798698938395775ffdbc63b25bb1c90b1
Павленков Николай <npavlenkov@gmail.com>
2021-04-21 14:25:12 +0300

    progress

--- src/test/java/tasknew5.java
+++ src/test/java/tasknew5.java
@@ -1,2 +1,27 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class tasknew5 {
+    String
+    repos = "npavlenkov/tasks",
+    issuesnomer = "#1";
+    @Test
+    public void test5() {
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repos);
+        $(".header-search-input").submit();
+        $(By.linkText(repos)).click();
+        $(withText("Issues")).click();
+        $(withText(issuesnomer)).should(Condition.exist);
+
+
+        sleep(5000);
+
+    }
 }
